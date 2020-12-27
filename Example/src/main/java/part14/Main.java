fefb70267eea93eabcb85e8184e490a4a4014fb3
aiden.sim <aiden.sim@kakaocorp.com>
2020-12-28 03:06:49 +0900

    example

--- Example/src/main/java/part14/Main.java
+++ Example/src/main/java/part14/Main.java
@@ -1,4 +1,26 @@
 package part14;
 
+import part14.common.ArgsException;
+import part14.step1.Args;
+
+import java.text.ParseException;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            Args arg = new Args("l,p#,d*", args);
+            boolean logging = arg.getBoolean('l');
+            int port = arg.getInt('p');
+            String directory = arg.getString('d');
+            executeApplication(logging, port, directory);
+        } catch (ArgsException e) {
+            System.out.printf("Argument error: %s\n", e.errorMessage());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void executeApplication(boolean logging, int port, String directory) throws ArgsException {
+
+    }
 }
--- Example/src/main/java/part14/common/ArgsException.java
+++ Example/src/main/java/part14/common/ArgsException.java
@@ -1,4 +1,14 @@
-package part14;
+package part14.common;
 
-public class ArgsException {
+public class ArgsException extends Exception {
+    public ArgsException() {
+    }
+
+    public ArgsException(String message) {
+        super(message);
+    }
+
+    public Object errorMessage() {
+        return null;
+    }
 }
