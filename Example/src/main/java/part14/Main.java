37d67fb166f51979c8e53acf1ca2cfe3afabe630
aiden.sim <s860620@gmail.com>
2021-01-01 01:49:25 +0900

    part15

--- Example/src/main/java/part14/Main.java
+++ Example/src/main/java/part14/Main.java
@@ -9,7 +9,7 @@ import java.text.ParseException;
  * -l true -p 1000 -d test
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try {
             Args arg = new Args("l,p#,d*", args);
             boolean logging = arg.getBoolean('l');
